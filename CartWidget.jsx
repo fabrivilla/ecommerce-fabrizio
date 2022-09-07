4302376e95a5c758fbb1f93bbb8e2cc46c3790fa
Fabrizio Villa <101836376+fabrivilla@users.noreply.github.com>
2022-09-07 00:12:45 -0300

    Add files via upload

---
+++ CartWidget.jsx
@@ -0,0 +1,15 @@
+
+import { BsCart } from 'react-icons/fa/';
+import '../components/CartWidget.css'
+
+    
+
+
+export const CartWidget = () => {
+  return (
+    <div className='widget-cart'>
+        <BsCart />
+
+    </div>
+  )
+}
---
+++ NavBar.jsx
@@ -0,0 +1,27 @@
+import { CartWidget } from './CartWidget'
+import './NavBar.css'
+
+
+export const NavBar = () => {
+  return (
+    
+    <header>
+      Envíos a todo el pais. Pagá con el método de pago que prefieras.
+   
+    <ul>
+    <li>SNEAKERS</li>
+    <li>CLOTHES</li>
+    <li>ACCESORIES</li>
+    <li>US</li>
+
+    <CartWidget/>
+    
+   
+    
+    
+    
+    </ul>
+    
+    </header>
+  )
+}
